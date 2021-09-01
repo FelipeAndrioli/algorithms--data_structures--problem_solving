89996980f08ca3c38b722959168631d7be1857b9
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-09-01 07:51:01 -0300

    Added binary search algorithm

--- algorithms/search_algorithms/binary_search.cpp
+++ algorithms/search_algorithms/binary_search.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 
-int binary_search(int *array, int array_size, int value) {
-    int index = -1;
+int binary_search(int *array, int value, int left, int right) {
 
-    return index;
+    int not_exist = -1;
+
+    if (left > right) {
+        return not_exist;
+    }
+
+    int mid = left + ((right - left) / 2);
+
+    if (array[mid] == value) {
+        return mid;
+    } else if (array[mid] > value) {
+        return binary_search(array, value, left, mid - 1);
+    } else if (array[mid] < value) {
+        return binary_search(array, value, mid + 1, right);
+    }
+
+    return not_exist;
 }
 
 int main() {
 
-    int numbers_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int numbers_array[] = {1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
     int array_size = sizeof(numbers_array) / sizeof(*numbers_array);
-    int value = 3;
+    int value = 13;
 
+    std::cout << array_size << std::endl;
     std::cout << "Looking for value " << value << std::endl;
-    int result = binary_search(numbers_array, array_size, value);
+    int result = binary_search(numbers_array, value, 0, array_size);
 
     if (result == -1) {
         std::cout << "Value does not exist in the array" << std::endl;
     } else {
         std::cout << "Value in position " << result << " of the array" << std::endl;
+        std::cout << "Printing value " << result << " of the array => " << numbers_array[result] << std::endl;
     }
 
     return 0;
