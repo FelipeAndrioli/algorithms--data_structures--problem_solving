3db75d6f4c897a9be67af20828cd97a52834a82d
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-09-08 11:28:17 -0300

    Added circular queue data structure

---
+++ lib_features/data_structures/circular_queue.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+
+class Circular_Queue {
+    private:
+        std::vector<int> data;
+        int head;
+        int tail;
+        int size;
+    public:
+        Circular_Queue(int queue_size);
+        bool is_empty();
+        bool is_full();
+        int front();
+        int rear();
+        bool enqueue(int value);
+        bool dequeue();
+};
+
+Circular_Queue::Circular_Queue(int queue_size) {
+    data.resize(queue_size);
+    head = -1;
+    tail = -1;
+    size = queue_size;
+}
+
+bool Circular_Queue::is_empty() {
+    if (head == -1) {
+        return true;
+    }
+
+    return false;
+}
+
+bool Circular_Queue::is_full() {
+    if ((tail + 1) % size == head) {
+        return true;
+    }
+
+    return false;
+}
+
+int Circular_Queue::front() {
+    if (is_empty()) {
+        return -1;
+    }
+
+    return data[head];
+}
+
+int Circular_Queue::rear() {
+    if (is_empty()) {
+        return -1;
+    }
+
+    return data[tail];
+}
+
+bool Circular_Queue::enqueue(int value) {
+    if (is_full()) {
+        return false;
+    }
+
+    if (is_empty()) {
+        head = 0;
+    }
+
+    tail = (tail + 1) % size;
+    data[tail] = value;
+
+    return true;
+}
+
+bool Circular_Queue::dequeue() {
+    if (is_empty()) {
+        return false;
+    }
+
+    if (head == tail) {
+        head = -1;
+        tail = -1;
+        return true;
+    }   
+
+    head = (head + 1) % size;
+
+    return true;
+}
+
+int main() {
+
+    Circular_Queue *circular_queue = new Circular_Queue(3);
+
+    std::cout << circular_queue->enqueue(1) << std::endl;   //1
+    std::cout << circular_queue->enqueue(2) << std::endl;   //1
+    std::cout << circular_queue->enqueue(3) << std::endl;   //1
+    std::cout << circular_queue->enqueue(4) << std::endl;   //1
+    std::cout << circular_queue->rear() << std::endl;       //
+    std::cout << circular_queue->is_full() << std::endl;       //
+    std::cout << circular_queue->dequeue() << std::endl;    //1
+    std::cout << circular_queue->enqueue(4) << std::endl;   //1
+    std::cout << circular_queue->rear() << std::endl;       //
+    
+    return 0;
+}
