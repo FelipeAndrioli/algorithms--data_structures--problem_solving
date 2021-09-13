24ac05c2651dd0d939abc328ca195acc52fe61cb
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-09-13 11:17:32 -0300

    Added inverted star pyramid variation

--- algorithms/patterns/inverted_star_pyramid.cpp
+++ algorithms/patterns/inverted_star_pyramid.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 
-int main() {
-
-    int number_of_rows;
-
-    std::cout << "Enter the number of lines: ";
-    std::cin >> number_of_rows;
-
+void inverted_star_pyramid_pattern(int number_of_rows) {
     for (int i = number_of_rows; i >= 0; i--) {
         for (int s = i; s < number_of_rows; s++) {
             std::cout << " ";
@@ -18,6 +12,31 @@ int main() {
 
         std::cout << std::endl;
     }
+}
+
+void inverted_star_pyramid_pattern_variation(int number_of_rows) {
+    for (int i = number_of_rows; i >= 0; i--) {
+        for (int s = i; s < number_of_rows; s++) {
+            std::cout << " ";
+        }
+
+        for (int j = 0; j < i; j++) {
+            std::cout << "* ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+
+    int number_of_rows;
+
+    std::cout << "Enter the number of lines: ";
+    std::cin >> number_of_rows;
+
+    inverted_star_pyramid_pattern(number_of_rows);
+    inverted_star_pyramid_pattern_variation(number_of_rows);
 
     return 0;
 }
