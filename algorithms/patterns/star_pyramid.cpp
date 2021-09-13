2130f140b566cf4ac7795bfe53b38bf6940a2c4a
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-09-13 11:20:28 -0300

    Added star pyramid pattern variation

--- algorithms/patterns/star_pyramid.cpp
+++ algorithms/patterns/star_pyramid.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 
-int main() {
-
-    int number_of_rows;
-
-    std::cout << "Enter the number of rows: ";
-    std::cin >> number_of_rows;
-
+void star_pyramid_pattern(int number_of_rows) {
     for (int i = 1; i <= number_of_rows; i++) {
         for (int s = i; s < number_of_rows; s++) {
             std::cout << " ";
@@ -17,6 +11,31 @@ int main() {
         }
         std::cout << std::endl;
     }
+}
+
+void star_pyramid_patter_variation(int number_of_rows) {
+    for(int i = 1; i <= number_of_rows; i++) {
+        for (int s = i; s < number_of_rows; s++) {
+            std::cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            std::cout << "* ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+
+    int number_of_rows;
+
+    std::cout << "Enter the number of rows: ";
+    std::cin >> number_of_rows;
+
+    star_pyramid_pattern(number_of_rows);
+    star_pyramid_patter_variation(number_of_rows);
 
     return 0;
 }
