7c2edb94ecb57de5a9ed3cf804227d35946c3c4c
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-10-05 11:45:26 -0300

    Added bottom-up dynamic solution

--- problem_solving/leetcode/fibonacci.cpp
+++ problem_solving/leetcode/fibonacci.cpp
@@ -15,16 +15,33 @@ int fib_memoize() {
     return 0;
 }
 
-int fib_bottom_up() {
-    return 0;
+int fib_bottom_up(int n) {
+
+    int memo[n] = {};
+
+    if (n == 0 || n == 1) {
+        return n;
+    }
+
+    memo[0] = 0;
+    memo[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        memo[i] = memo[i - 1] + memo[i - 2];
+    }
+
+    return memo[n];
 }
 
 int main() {
 
     int n = 0;
+    int memo[] = {};
 
     cout << "Enter the number to calculate F(n): ";
     cin >> n;
+
+    cout << fib_bottom_up(n) << endl;
 
     return 0;
 }
