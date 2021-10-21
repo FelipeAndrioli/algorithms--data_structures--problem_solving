ebf0654cdfe1caa40596f453c3d92ad1d80559cc
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-10-21 14:28:39 -0300

    Added the definitive solution to tape equilibrium challenge

--- problem_solving/codility/tape_equilibrium.cpp
+++ problem_solving/codility/tape_equilibrium.cpp
@@ -6,36 +6,42 @@
 using namespace std;
 
 int solution(vector<int> &A) {
+	int minimum_difference = INT_MAX;
+	int total_sum = 0;
+	int left = 0;
+	int right = 0;
 
-	int minimum_diff = INT_MAX;
-
-	for (int i = 0; i < A.size() - 1; i++) {
-		int left = 0;
-		int right = 0;
-
-		for (int j = 0; j < A.size(); j++) {
-			if (j <= i) {
-				left += A[j];
-			} else {
-				right += A[j];
-			}
-		}
-
-		int temp_diff = left - right;
-
-		temp_diff = abs(temp_diff);
-
-		minimum_diff = min(minimum_diff, temp_diff);
+	for (int i = 0; i < A.size(); i++) {
+		total_sum += A[i];
 	}
 
-	return minimum_diff;
+	for (int i = 0; i < A.size() - 1; i++) {
+		left += A[i];
+		right = total_sum - left;
+		
+		int temp_min = left - right;
+
+		temp_min = abs(temp_min);
+
+		if (temp_min < minimum_difference) {
+			minimum_difference = temp_min;
+		}
+	}
+
+	return minimum_difference;
 }
 
 int main() {
 
 	//vector<int> A = {3, 1, 2, 4, 3};
 	
-	vector<int> A = {-1000, 1000};
+	//vector<int> A = {-1000, 1000};
+
+	//vector<int> A = {1, 1, 3};
+	
+	//vector<int> A = {2, 4, 6, 8};
+
+	vector<int> A = {-2, -3, -4, -1};
 
 	cout << solution(A) << endl;
 
@@ -92,10 +98,17 @@ int main() {
 		the array value in left, else we add in right. The following steps are take the absolute difference and compare
 		with the previous value we have.
 
+		The second solution was to loop once through the entire array summing each value. After that we iterate again
+		through the array keeping track of left part of the array, adding the current value to the left var. The
+		right part of the array we calculate subtracting the left from the total value.
+
 		Complexity
 
 		My first solution wasn't very good, once it need to iterate through the array for each array value resulting
 		in a complexity of O(n^2).
+
+		The second solution loop once through the entire array O(n) to sum all the elements. And then loop the second
+		time to calculate the difference O(n). In the end we are going to have O(2 * n) which is transformed in O(n).
 
 	 * */
 
