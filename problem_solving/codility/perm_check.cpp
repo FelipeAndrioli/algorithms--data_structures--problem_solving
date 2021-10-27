2be864e0b8dd0ef291ff4f8f6e7f6dfd0485cd4a
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-10-26 22:34:47 -0300

    Added a small code correction and comments about the solution and the complexity

--- problem_solving/codility/perm_check.cpp
+++ problem_solving/codility/perm_check.cpp
@@ -5,24 +5,21 @@ using namespace std;
 
 int solution(vector<int> &A) {
 
-	set<int> N;
-	set<int>::iterator it;
+	map<int, int> N;
+	map<int, int>::iterator it;
 
 	for (int i = 0; i < A.size(); i++) {
-		N.insert(A[i]);
+		N[A[i]]++;	
 	}
 	
 	int check = 0; 
 
 	for (it = N.begin(); it != N.end(); it++) {
-
-		cout << *it << " : " << check << endl;
-
-		if (*it != check + 1) {
+		if (it->first != check + 1 || it->second != 1) {
 			return 0;
 		}
 
-		check = *it;
+		check = it->first;
 	}
 
 	return 1;
@@ -30,8 +27,8 @@ int solution(vector<int> &A) {
 
 int main() {
 
-//	vector<int> A = {4, 1, 3, 2};
-	vector<int> A = {2};
+	vector<int> A = {4, 1, 3, 2};
+//	vector<int> A = {1, 1};
 
 	cout << solution(A) << endl;
 
@@ -84,10 +81,16 @@ int main() {
 	each element of array A is an integer within the range [1..1,000,000,000].
 
 	Solution:
+	
+	My first solution was already the optimized one. We need to loop through the entire array once and add each
+	element to a map, with its occurrencies. Then we need to loop through the map verifying if the sequence is
+	correct and if the value is not occurring more than one time.
 
 	Complexity:
 
-
+	Doing it with C++ libraries we got a O(n) complexity, once we need to loop through the array once and through the
+	map once. But if we haven't the libraries we probably are going to need to order and count by ourselves. This would
+	give us a time complexity of O(n * log n).
  * */
 
 	return 0;
