f64d7e2638d3d2cb7c44bbcf538a8d8eb5f3f519
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-12-28 09:37:54 -0300

    Added recursive solution

--- problem_solving/leetcode/binary_search.cpp
+++ problem_solving/leetcode/binary_search.cpp
@@ -3,6 +3,49 @@
 
 using namespace std;
 
+int recursiveBinarySearch(vector<int>& nums, int target, int left, int right) {
+	int mid = left + (right - left) / 2;
+	int search_result = -1;
+
+	if (nums[mid] == target) {
+		return mid;
+	}
+
+	if (nums[mid + 1] <= target) {
+		search_result = recursiveBinarySearch(nums, target, mid + 1, right);
+	} else if (nums[mid - 1] >= target) {
+		search_result = recursiveBinarySearch(nums, target, left, mid - 1);
+	}
+
+	return search_result;
+}
+
+int search(vector<int>& nums, int target) {	
+	if (nums.size() == 0) {
+		return -1;
+	} else if (nums.size() == 1) {
+		if (nums[0] == target) {
+			return 0;
+		} else {
+			return -1;
+		}
+	} else if (nums.size() == 2) {
+		if (nums[0] == target) {
+			return 0;
+		} else if (nums[1] == target) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	if (target > nums[nums.size() -1] || target < nums[0]) {
+		return -1;
+	}
+
+	return recursiveBinarySearch(nums, target, 0, nums.size() - 1);
+}
+
 int nonRecursiveBinarySearch(vector<int>& nums, int target) {
 
 	if (nums.size() == 0) {
@@ -47,11 +90,12 @@ int nonRecursiveBinarySearch(vector<int>& nums, int target) {
 
 int main() {
 
-	vector<int> nums = {2, 5};
-	int target = 0; 
+	vector<int> nums = {-1,0,3,5,9,12};
+	int target = 13; 
 
 	cout << "Binary search...." << endl;
-	cout << nonRecursiveBinarySearch(nums, target) << endl;
+	//cout << nonRecursiveBinarySearch(nums, target) << endl;
+	cout << search(nums, target) << endl;
 
 	return 0;
 }
